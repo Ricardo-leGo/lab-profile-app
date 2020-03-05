d87ba5a808c32f5ab7cc732bb15362ff7c43febe
Ricardo <ricnoceronte@gmail.com>
2020-03-05 06:57:20 -0600

    final commit

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin:'http://localhost:3001',
+    origin:process.env.ORIGIN,
     credentials: true
   }))
   app.use(flash());
